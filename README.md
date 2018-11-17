a99b26efd46471c1925ca686f083576a4455d014
Benstrocity <byoungblood.c@gmail.com>
2018-11-17 06:30:26 -0600

    Fixed issue with different categories not displaying random output, removed loader (will re-add), cleaned javascript code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # dowhat
-This is the first stable release for DoWhat
+DoWhat is an app designed to change things up a little. If you're bored, fire up DoWhat and change your plans for the day.
--- js/game.js
+++ js/game.js
@@ -1,66 +1,79 @@
 /*
     https://github.com/Benstrocity/dowhat.git
+    
+    DoWhat is an app designed to change things up a little. If you're bored, fire up DoWhat and change your plans for the day.
 */
-var freeArr = ['Free 1', 'Free 2', 'Free 3', 'Free 4', 'Free 5'];
-var outArr = ['Out 1', 'Out 2', 'Out 3', 'Out 4', 'Out 5'];
-var foodArr = ['Food 1', 'Food 2', 'Food 3', 'Food 4', 'Food 5'];
-var entArr = ['Free 1', 'Free 2', 'Free 3', 'Free 4', 'Free 5'];
-var doWhat = '';
+
+//Defining global scope variables and objects
+var arrObj = {
+    free : ['Free 1', 'Free 2', 'Free 3', 'Free 4', 'Free 5'],
+    outdoors : ['Out 1', 'Out 2', 'Out 3', 'Out 4', 'Out 5'],
+    food : ['Food 1', 'Food 2', 'Food 3', 'Food 4', 'Food 5'],
+    entertain : ['Ent 1', 'Ent 2', 'Ent 3', 'Ent 4', 'Ent 5']
+};
+var paraOutput = document.querySelector('#para-output');
 var displayDoWhat = document.querySelector('#output');
 var startOne = document.querySelector('#start-free');
 var startTwo = document.querySelector('#start-out');
 var startThree = document.querySelector('#start-food');
 var startFour = document.querySelector('#start-ent');
-var loading = document.querySelector('#timer');
+var startButtons = document.querySelector('.start');
+var refresh = document.querySelector('#refresh-app');
 
-startDisplay();
-getDoWhat();
-
+//Function that starts the program. Function called at bottom of code
 function startDisplay () {
-    startOne.style.display = "block";
-    startTwo.style.display = "block";
-    startThree.style.display = "block";
-    startFour.style.display = "block";
+    startButtons.style.display = 'block';
     displayDoWhat.style.display = "none";
-    loading.style.display = "none";
 }
 
-function getDoWhat () {
-    var randNum = Math.floor(Math.random() * 5);
-    var i = randNum;
-    if (getDoWhat == startOne) {
-    doWhat += "<h2>" + freeArr[i] + "</h2>";
-    document.getElementById('do-what').innerHTML = doWhat; 
-    }
-}
-
-function startTimer () {
-    startOne.style.display = "none";
-    startTwo.style.display = "none";
-    startThree.style.display = "none";
-    startFour.style.display = "none";
-    loading.style.display = "block";
-    
-    var timeLeft = 1;
-    var timer = setInterval(function(){
-    document.getElementById("progressBar").value = 1 - --timeLeft;
-    if(timeLeft === -1){ 
-        updateDisplay();
-    }
-    },1000);
-}
-
-function updateDisplay () {
+//Functions to randomize based on button click
+function updateOne () {
+    startButtons.style.display = "none";
     displayDoWhat.style.display = "block";
-    document.getElementById('refresh-app').style.display = "block";
-    document.getElementById('timer').style.display = "none";
+    refresh.style.display = "block";
+    
+    var i = Math.floor(Math.random() * 5); //Randomizes a number
+    paraOutput.innerHTML = arrObj.free[i]; //Applies random number to array item to display
 }
 
+function updateTwo () {
+    startButtons.style.display = "none";
+    displayDoWhat.style.display = "block";
+    refresh.style.display = "block";
+    
+    var i = Math.floor(Math.random() * 5); //Randomizes a number
+    paraOutput.innerHTML = arrObj.outdoors[i]; //Applies random number to array item to display
+}
+
+function updateThree () {
+    startButtons.style.display = "none";
+    displayDoWhat.style.display = "block";
+    refresh.style.display = "block";
+    
+    var i = Math.floor(Math.random() * 5); //Randomizes a number
+    paraOutput.innerHTML = arrObj.food[i]; //Applies random number to array item to display
+}
+
+function updateFour () {
+    startButtons.style.display = "none";
+    displayDoWhat.style.display = "block";
+    refresh.style.display = "block";
+    
+    var i = Math.floor(Math.random() * 5); //Randomizes a number
+    paraOutput.innerHTML = arrObj.entertain[i]; //Applies random number to array item to display
+}
+
+//Function to reload the program when the appropriate element is clicked to refresh the page
 function updateReload () {
     window.location.reload();
 }
 
-startOne.addEventListener('click', startTimer);
-startTwo.addEventListener('click', startTimer);
-startThree.addEventListener('click', startTimer);
-startFour.addEventListener('click', startTimer);
+//Click behavior event listeners
+startOne.addEventListener('click', updateOne);
+startTwo.addEventListener('click', updateTwo);
+startThree.addEventListener('click', updateThree);
+startFour.addEventListener('click', updateFour);
+refresh.addEventListener('click', updateReload);
+
+//Start code on load
+startDisplay();
